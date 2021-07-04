07ba1a73d20a71a91fc81fbe023b12dc468068fc
Miguel Panelo <miguel.panelo@persgroep.net>
2021-07-04 16:55:09 +0200

    Update theming and ui skeleton

--- app/src/main/java/dev/gumil/rooms/MainActivity.kt
+++ app/src/main/java/dev/gumil/rooms/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.gumil.rooms.ui.theme.RoomsTheme
 
@@ -15,24 +18,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RoomsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                RoomsApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RoomsApp() {
+    RoomsTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                })
+            },
+            content = {
+                Surface(color = MaterialTheme.colors.background) {
+                    Text(text = "Hello World")
+                }
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    RoomsTheme {
-        Greeting("Android")
-    }
+    RoomsApp()
 }
